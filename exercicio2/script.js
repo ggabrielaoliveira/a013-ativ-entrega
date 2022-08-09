4cfe5727cac9849ca8c376b6a24a2f20f7f32d11
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-08-09 09:12:27 -0300

    Exercicio2 finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,43 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
-
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
-}
+let nacionalidade= prompt("Escreva sua nacionalidade aqui!").toLowerCase();
+//
+switch(nacionalidade){
+    case "brasileira":
+    case "brasileiro":
+        console.log("Você nasceu no Brasil")
+        break;
+    case "argentina":
+    case "argentino":
+        console.log("Você nasceu na Argentina")
+        break;
+    case "uruguaia":
+    case "uruguaio":
+        console.log("Você nasceu no Uruguai")
+        break;
+    case "chilena":
+    case "chileno":
+        console.log("Você nasceu no Chile")
+        break;
+    case "colombiana":
+    case "colombiano":
+        console.log("Você nasceu na Colombia")
+        break;
+    default:
+        console.log("Nacionalidade não encontrada")
+        
+};
+// if(nacionalidade=="brasileira"|| nacionalidade=="brasileiro"){
+//     console.log("sua nacionalidade foi aceita")
+// }else{}
+// if(nacionalidade=="argentina"|| nacionalidade=="argentino"){
+//     console.log("sua nacionalidade foi aceita")
+// }else{}
+// if(nacionalidade=="uruguaia"|| nacionalidade=="uruguaio"){
+//     console.log("sua nacionalidade foi aceita")
+// }else{}
+// if(nacionalidade=="chilena"|| nacionalidade=="chileno"){
+//     console.log("sua nacionalidade foi aceita")
+// }else{}
+// if(nacionalidade=="colombiana"|| nacionalidade=="colombiano"){
+//     console.log("sua nacionalidade foi aceita")
+// }else{
+//     console.log("nacionalidade não encontrada :(")};
