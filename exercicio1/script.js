19747d735e38b814c8e2546b83c5329f7e9958b8
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-08-09 07:59:15 -0300

    exercicio1 realizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,18 @@
-const num = //seu numero aqui
-
+const num = Number(prompt("Insira um numero aqui"));
+// ANINHADO
+if(num%2==0){
+    console.log("Numero divisivel por 2")
+        if(num%3==0){
+            console.log("Numero divisivel por 3")  
+        }else{
+            console.log("numero não é divisivel por 3")
+        }
+}else{
+    console.log("numero não é divisivel por 2")
+};
+// OPERADOR LOGICO
+if(num%2==0||num%3==0){
+    console.log("numero divisivel por 2 ou3")
+}else{
+    console.log("numero não divisivel por 2 ou 3")
+};
